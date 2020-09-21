db2c8c537cbad983b93ac6dd3d71fe45240711fe
mwbrulhardt <mwbrulhardt@gmail.com>
2020-09-21 12:25:12 -0400

    Removed git submodule.

---
+++ canopy/__init__.py
@@ -0,0 +1,44 @@
+
+import json
+
+from canopy.client import ClientAPI
+
+
+def api(name: str, **kwargs) -> ClientAPI:
+
+    if name in ClientAPI.apis:
+        client = ClientAPI.apis[name]
+        if kwargs:
+            client.auth(**kwargs)
+        return client
+
+    path = __path__[0] + f"/registry/{name}.json"
+    client = ClientAPI.from_json(path)
+
+    if kwargs:
+        client.auth(**kwargs)
+
+    return client
+
+
+def from_json(path: str) -> ClientAPI:
+    """Generates a `ClientAPI` from a json configuration file.
+
+    Parameters
+    ----------
+    path : str
+        The path of the configuration file.
+
+    Returns
+    -------
+    `ClientAPI`
+        A client api generated from the configuration file.
+    """
+    config = json.load(open(path))
+    api = ClientAPI(
+        name=config['name'],
+        uri=config['uri'],
+        endpoints=config['endpoints'],
+        **config.get('session', {})
+    )
+    return api
---
+++ canopy/client.py
@@ -0,0 +1,108 @@
+
+import json
+import requests
+import string
+
+from requests import Session
+from requests.structures import CaseInsensitiveDict
+from typing import Callable
+
+
+class Endpoint:
+    """A class that provides the functionality of accessing and describing an
+    endpoint.
+
+    Parameters
+    ----------
+    api : `ClientAPI`
+        The client api that this endpoint belongs to.
+    info : dict
+        Information needed to make a successful request to the endpoint.
+    """
+
+    def __init__(self, api: 'ClientAPI', info: dict) -> None:
+        self.api = api
+        self.info = info
+        self.url_template = string.Template(info["path"])
+
+    def __call__(self, **kwargs) -> dict:
+        url_params = kwargs.pop('url_params', {})
+        kwargs = {**self.info.get("kwargs", {}), **kwargs}
+
+        response = self.api.session.request(
+            method=self.info["method"],
+            url=self.url_template.substitute(**url_params),
+            **kwargs
+        )
+
+        response.raise_for_status()
+
+        try:
+            data = response.json()
+        except ValueError:
+            data = response.content
+
+        return data
+
+
+class ClientAPI:
+    """"A class for building any client api using the information about all of
+    their endpoints.
+
+    Parameters
+    ----------
+    name : str
+        Name of the client api.
+    uri : str
+        Base uri to use for all endpoints in the api.
+    endpoints : dict
+        Available subapis and endpoints for the client.
+    kwargs : dict
+        Other persistent information to intialize the `Session`.
+    """
+    
+    apis = {}
+
+    def __init__(self,
+                 name: str,
+                 uri: str,
+                 endpoints: dict,
+                 **kwargs) -> None:
+        self.name = name
+        self.uri = uri
+
+        if self.name not in ClientAPI.apis.keys():
+            self.apis[self.name] = self
+            self.session = Session()
+            self.persist(**kwargs)
+
+        for k, info in endpoints.items():
+            if "path" in info.keys():
+                endpoint = Endpoint(
+                    api=ClientAPI.apis[self.name],
+                    info=info
+                )
+                setattr(self, k, endpoint)
+            else:
+                api = ClientAPI(name, uri, info)
+                setattr(self, k, api)
+
+    def persist(self, **kwargs) -> None:
+        """Persists given parameters for the client session."""
+        for k in Session.__attrs__:
+
+            if k in kwargs.keys():
+                v = getattr(self.session, k)
+
+                if k == "headers":
+                    v = dict(v, **CaseInsensitiveDict(kwargs[k]))
+                elif isinstance(v, dict):
+                    v = dict(v, **kwargs[k])
+                else:
+                    v = kwargs.get(k)
+
+                setattr(self.session, k, v)
+
+    def auth(self, **kwargs) -> None:
+        """Persists authentication information for the client."""
+        self.persist(**kwargs)
