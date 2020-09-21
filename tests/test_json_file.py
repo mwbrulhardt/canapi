f7be12d20c48abb2a248f93194195a6227f52aa6
mwbrulhardt <mwbrulhardt@gmail.com>
2020-09-21 12:20:01 -0400

    Added first iteration of canopy.

---
+++ tests/test_json_file.py
@@ -0,0 +1,21 @@
+
+import canopy as cp
+
+
+def test_polygon():
+    polygon = cp.api("polygon")
+    assert polygon
+
+def test_estimize():
+    estimize = cp.api("estimize")
+    assert estimize
+
+
+def test_iex():
+    iex = cp.api("iexcloud")
+    assert iex
+
+
+def test_flightaware():
+    flightaware = cp.api("flightaware")
+    assert flightaware
