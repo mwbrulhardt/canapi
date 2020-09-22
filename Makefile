55cf3940ea89c6ae5ec2b99da7f1af6d9d6475e9
mwbrulhardt <mwbrulhardt@gmail.com>
2020-09-22 15:36:53 -0400

    Filled out setup.py file and added Makefile.

---
+++ Makefile
@@ -0,0 +1,10 @@
+package:
+	  rm -rf dist
+	  python3 setup.py sdist
+	  python3 setup.py bdist_wheel
+
+test-release: package
+	  python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+release: package
+	  python3 -m twine upload dist/*
---
+++ setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="canopy",
+    version="0.0.1",
+    description="A universal client api generator.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/finverse/canopy",
+    author="Matthew Brulhardt",
+    author_email="mwbrulhardt@finverselabs.com",
+    license="Apache 2.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True
+)
