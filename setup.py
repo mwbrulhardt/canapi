0c6ba8aefeef61be1bcb4880b542739306e32857
mwbrulhardt <mwbrulhardt@gmail.com>
2020-09-23 17:31:29 -0400

    Updated setup classifiers.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="canapi",
-    version="0.0.1",
+    version="0.0.2",
     description="A universal client api generator.",
     long_description=README,
     long_description_content_type="text/markdown",
